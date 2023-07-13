8881f65c1ac1a1621d856d6b4c5e9deeebba266f
Software engeneer <kikonistephane@gmail.com>
2023-07-13 19:01:38 +0200

    sata

--- src/app/pages/forms/FormJeuneTalent.js
+++ src/app/pages/forms/FormJeuneTalent.js
@@ -24,21 +24,21 @@ const FormJeuneTalent = ({ setNom, setEmail, setNumTel, setCategorie,
 
     return (
         <div className='formulaire'>
-            <div className='row mb-3'>
-                <div className="col-sm-6">
+            <div className='row'>
+                <div className="col-sm-6 mb-3">
                     <input type="text" className="form-control"
                         onChange={(e) => setNom(e.target.value)}
                         placeholder="Votre nom*" />
                 </div>
-                <div className="col-sm-6">
+                <div className="col-sm-6 mb-2">
                     <input type="text" className="form-control"
                         onChange={(e) => setPrenom(e.target.value)}
                         placeholder="Votre prénom*" />
                 </div>
             </div>
 
-            <div className='row mb-3'>
-                <div className="col-sm-6 mt-2">
+            <div className='row'>
+                <div className="col-sm-6 mt-2  mb-2">
                     <input type="text" className="form-control"
                         placeholder='Votre adresse email'
                         onChange={(e) => setEmail(e.target.value)}
@@ -51,22 +51,22 @@ const FormJeuneTalent = ({ setNom, setEmail, setNumTel, setCategorie,
                 </div>
             </div>
 
-            <div className='row mb-3'>
-                <label>Votre date de naissance*</label>
-                <div className="col-sm-6 mt-2">
+            <div className='row '>
+                <label className='lableDate'>Votre date de naissance*</label>
+                <div className="col-sm-6 mt-2 mb-2">
                     <input type="date" className="form-control"
                         onChange={(e) => setDateNaissance(e.target.value)}
                     />
                 </div>
-                <div className="col-sm-6 mt-2">
-                    <input type="text" className="form-control"
+                <div className="col-sm-6 mt-2 mb-1">
+                    <input type="text" className="form-control mb-3"
                         onChange={(e) => setCommune(e.target.value)}
                         placeholder="Votre commune*" />
                 </div>
             </div>
 
-            <div className='row mb-3'>
-                <div className="col-sm-6">
+            <div className='row'>
+                <div className="col-sm-6  mb-3">
                     <input type="text" className="form-control"
                         onChange={(e) => setOccupation(e.target.value)}
                         placeholder="Votre occupation*" />
@@ -80,7 +80,7 @@ const FormJeuneTalent = ({ setNom, setEmail, setNumTel, setCategorie,
                 </div>
             </div>
 
-            <label>Télécharger une vidéo à nous envoyer</label>
+            <label className='lableDate'>Télécharger une vidéo à nous envoyer</label>
             <div className='row mt-2'>
                 <div className="col-sm-12">
                     <input
